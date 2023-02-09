30901292e5ac25fc8fcfc6ccce8f238a51b79c56
swe <newbornking999@gmail.com>
2023-02-09 08:43:18 +0000

    main.py

--- resnet/main.py
+++ resnet/main.py
@@ -27,7 +27,7 @@ import copy
 #load_data
 def load_data():
     # if not exists the path, make the directory
-    path2data = '/data/emocog_data/data/stl'
+    path2data = '/media/jk_dataset/emocog_data/data/stl'
 
     if not os.path.exists(path2data):
         os.mkdir(path2data)
@@ -38,16 +38,40 @@ def load_data():
 
     print(len(train_ds))
     print(len(val_ds))
-    print(np.array(train_ds)[0][0].shape)
+    #print(np.array(train_ds)[0][0].shape)
 
     return train_ds, val_ds
 
 def calculate_normalize(train_ds, val_ds):
     
     # To normalize the dataset, calculate the mean and std
+    # To normalize the dataset, calculate the mean and std
+    x, _ = train_ds[0]
+    print('train_ds->',np.array(x).shape)
     train_meanRGB = [np.mean(x.numpy(), axis=(1,2)) for x, _ in train_ds]
     train_stdRGB = [np.std(x.numpy(), axis=(1,2)) for x, _ in train_ds]
-    print(f'train_meanRGB->{train_meanRGB}')
+
+    train_meanR = np.mean([m[0] for m in train_meanRGB])
+    train_meanG = np.mean([m[1] for m in train_meanRGB])
+    train_meanB = np.mean([m[2] for m in train_meanRGB])
+    train_stdR = np.mean([s[0] for s in train_stdRGB])
+    train_stdG = np.mean([s[1] for s in train_stdRGB])
+    train_stdB = np.mean([s[2] for s in train_stdRGB])
+
+
+    val_meanRGB = [np.mean(x.numpy(), axis=(1,2)) for x, _ in val_ds]
+    val_stdRGB = [np.std(x.numpy(), axis=(1,2)) for x, _ in val_ds]
+
+    val_meanR = np.mean([m[0] for m in val_meanRGB])
+    val_meanG = np.mean([m[1] for m in val_meanRGB])
+    val_meanB = np.mean([m[2] for m in val_meanRGB])
+
+    val_stdR = np.mean([s[0] for s in val_stdRGB])
+    val_stdG = np.mean([s[1] for s in val_stdRGB])
+    val_stdB = np.mean([s[2] for s in val_stdRGB])
+
+    print(train_meanR, train_meanG, train_meanB)
+    print(val_meanR, val_meanG, val_meanB)
 
 def main():
     train_ds, val_ds = load_data()
